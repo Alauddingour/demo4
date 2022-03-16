70ca3d63b0f275fe4941d3f8b6570891d731f633
Alauddingour <101685829+Alauddingour@users.noreply.github.com>
2022-03-16 22:46:22 +0530

    Add files via upload

---
+++ basic2.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class basic2{
+  boolean b;
+  int a;
+  float c=90;
+  double d;
+	public static void main(String[] args) 
+	{ 
+	  float r= 89.8f; // by default decimal is considered as double
+	  
+	basic2 t= new basic2();
+	 System.out.println(t.b);
+	 System.out.println(t.a);
+	 System.out.println(t.c);
+	 System.out.println(t.d);
+	 
+	 
+	}
+
+}
